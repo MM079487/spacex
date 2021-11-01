38f7e4ede29d1f45e9ae72c6252813d317ee554e
MM07 <71704916+MM079487@users.noreply.github.com>
2021-11-01 15:46:45 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -56,6 +56,7 @@ window.onload = function load(){
             wiki.href=data.links.wikipedia
             webcast.href=data.links.webcast
             patch.src = data.links.patch.small
+            console.log(data)
         })
         
     }
@@ -74,9 +75,8 @@ window.onload = function load(){
 
     function timeConverter(UNIX_timestamp){
         var a = new Date(UNIX_timestamp * 1000);
-        var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
         var year = a.getFullYear();
-        var month = months[a.getMonth()];
+        var month = a.getMonth();
         var date = a.getDate();
         var hour = a.getHours();
         var min = a.getMinutes();
